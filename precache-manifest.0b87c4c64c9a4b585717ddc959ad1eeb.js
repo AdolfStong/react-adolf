4db0de31b6c7b5a6765b0f1a2cda4b0afc594804
shentong <shentong@meishubao.com>
2020-03-06 19:19:56 +0800

    Updates

---
+++ precache-manifest.0b87c4c64c9a4b585717ddc959ad1eeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f994b55dfcfaf5a14ee92577afd6342e",
+    "url": "/react-adolf/index.html"
+  },
+  {
+    "revision": "df51976e026ffee893f0",
+    "url": "/react-adolf/static/css/main.b4265fd8.chunk.css"
+  },
+  {
+    "revision": "9f9d7feb918b15f3e078",
+    "url": "/react-adolf/static/js/2.cf79cf89.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-adolf/static/js/2.cf79cf89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df51976e026ffee893f0",
+    "url": "/react-adolf/static/js/main.21426f13.chunk.js"
+  },
+  {
+    "revision": "dd3018f3d0c244bac95f",
+    "url": "/react-adolf/static/js/runtime-main.2769583d.js"
+  }
+]);
